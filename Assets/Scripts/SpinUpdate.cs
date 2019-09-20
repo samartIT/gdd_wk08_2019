5e85d0fe3b69284e1b0ef139d452f160fdae1002
samartIT <41602825+samartIT@users.noreply.github.com>
2019-09-20 20:19:39 +0700

    init unity project

---
+++ Assets/Scripts/SpinUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinUpdate : MonoBehaviour
+{
+    public float speed = 50;
+    public Vector3 rotationAxes = new Vector3(0, 1, 0);
+    void Update()
+    {
+        transform.Rotate(rotationAxes, speed * Time.deltaTime);
+    }
+}
