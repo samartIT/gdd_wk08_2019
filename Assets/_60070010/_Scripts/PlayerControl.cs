883b099b5f5a3c380d4a6c28fbcad7fe3f072a36
petolunn <petolunn@users.noreply.github.com>
2019-09-30 15:37:49 +0700

    L8

---
+++ Assets/_60070010/_Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+	public float driveSpeed = 30;
+	public float turnSpeed = 90f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * driveSpeed * verticalInput * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
