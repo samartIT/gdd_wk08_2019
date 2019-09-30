ef0f0d1889832d79214057dc8a0634f064fa11bb
PrintTrd <60070034@kmitl.ac.th>
2019-09-30 15:21:05 +0700

    Control the car

---
+++ Assets/_60070034/_Script/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset = new Vector3(17, 12, -33);
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation = player.transform.rotation;
+        transform.position = player.transform.position + offset;
+    }
+}
