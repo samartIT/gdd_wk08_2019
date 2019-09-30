af4161041b70ebda77212b831ad7fcc3c5b41102
FOR.FOSE <32724812+Apisit60070114@users.noreply.github.com>
2019-09-30 14:52:19 +0700

    update

---
+++ Assets/race-track-lake/_Scripts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Scripts : MonoBehaviour
+{
+    public float driveSpeed = 30;
+    public float trunSpeed = 90f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Verti")
+    }
+}
