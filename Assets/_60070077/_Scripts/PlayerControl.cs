d6564a511ead5e48131da1cd9def71530c6f3f01
Yonlada077 <32953428+Yonlada077@users.noreply.github.com>
2019-09-30 15:56:13 +0700

    1

---
+++ Assets/_60070077/_Scripts/PlayerControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl: MonoBehaviour
+{
+    public float driveSpeed = 30;
+    public float turnSpeed = 90f;
+    // Start is called before the first frame update
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * driveSpeed * verticalInput * Time.deltaTime);
+        
+    }
+}
