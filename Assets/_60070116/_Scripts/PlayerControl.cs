92b6cc23feaefde6964cea92fe238f7cb740fe19
attawut arpornrat <60070116@kmitl.ac.th>
2019-09-30 15:46:14 +0700

    edit

--- Assets/_60070116/_Scripts/PlayerControl.cs
+++ Assets/_60070116/_Scripts/PlayerControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float driveSpeed = 15;
+    public float driveSpeed = 30;
     public float turnSpeed = 90f;
     // Start is called before the first frame update
     void Start()
