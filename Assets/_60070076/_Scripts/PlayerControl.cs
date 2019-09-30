fd26bf081987ccf86b283bac90dd58767bdab9b8
skefolksy <skefolksy@hotmail.com>
2019-09-30 15:43:53 +0700

    final lab work

---
+++ Assets/_60070076/_Scripts/PlayerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float driveSpeed = 30;
+    public float turnSpeed = 90f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float turn = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward*driveSpeed*verticalInput*Time.deltaTime);
+        transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+    }
+}
