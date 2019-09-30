bf2246481388083b0068904fd25137bd5231f3aa
์ีNutpapat <60070024@kmitl.ac.th>
2019-09-30 19:07:24 +0700

    Nutpapat

---
+++ Assets/_60070024/PlayerControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float driveSpeed = 30;
+    public float turnSpeed = 90f;
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up*turnSpeed*horizontalInput*Time.deltaTime);
+        transform.Translate(Vector3.forward * driveSpeed * verticalInput*Time.deltaTime);
+    }
+}
