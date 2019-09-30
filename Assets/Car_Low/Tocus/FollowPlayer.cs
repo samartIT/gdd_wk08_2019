40b013ac71e435cbc362f282ff42f947635726d3
fernツ <32330533+fernarmyz@users.noreply.github.com>
2019-09-30 15:49:23 +0700

    update

---
+++ Assets/Car_Low/Tocus/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset = new Vector3(17, 12, -33);
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
