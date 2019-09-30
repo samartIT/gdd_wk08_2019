28408194ded18a5e08b9d927ba77bfa3d504cbe5
attawut arpornrat <60070116@kmitl.ac.th>
2019-09-30 15:24:25 +0700

    update

---
+++ Assets/_60070116/_Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float driveSpeed = 15;
+    public float turnSpeed = 90f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * driveSpeed * verticalInput * Time.deltaTime);
+        transform.Rotate(0, horizontalInput * turnSpeed * Time.deltaTime, 0);
+    }
+}
